99ddd03f7f8d99664fe26a11055ad0b3ad628c12
Doug Sweetser <sweetser@alum.mit.edu>
2020-02-12 22:32:14 -0500

    Updated documentation.

---
+++ quaternion_cross_product.md
@@ -0,0 +1,19 @@
+# Quaternion Cross Product
+
+Div, grad, curl, dot product, and cross product were all terms coined by one individual studying one thing: William 
+Rowan Hamilton in his study of quaternions. What all students study today is the convenient divorce granted by 
+Josiah Gibbs. Vector algebra is one of the most useful toolkits in one's mathematical collection. Vector algebra is
+such a dominant force in the mind's eye it may not be possible to image different perspectives on the subject. Yet that
+is my goal - not to say vector algebra is wrong in any way since there always necessarily is an equivalence relation
+between vector algebra and quaternions. Rather, one may gain new insights based on the the relationships between terms
+that are simply not there due to the divorce.
+
+What is a quaternion cross product? When one multiplies two quaternion together, it is the odd part of the product:
+
+st.latex(A B = (A B + B A)/2 + (A B - B A)/2 = Even(A B) + Odd(A B) )
+
+This is not the standard definition of a Gibbs cross product because there is no notion of the Even() product which
+happens to be a combination of the difference of a scalar times a scalar and  vector dot product summed with scalars 
+times vectors (translation: an abomination to those schooled in vector algebra).
+
+If one just focuses on the quaternion cross product, the scalar term 
--- webapp__lines_triangles_and_cross_products.py
+++ webapp__lines_triangles_and_cross_products.py
@@ -1,45 +1,62 @@
-import streamlit as sl
+import streamlit as st
 import plotly.graph_objects as go
+import textwrap
 
-from Qs import add, cross_q, dif, Qs, product, rotation_angle, norm_squared
+from Qs import add, cross_q, dif, Q, product, rotation_angle, norm_squared
 
 # Sidebar setup.
-x_1 = sl.sidebar.slider(label="P1_x", min_value=-2.0, max_value=2.0, value=1.0)
-y_1 = sl.sidebar.slider(label="P1_y", min_value=-2.0, max_value=2.0, value=1.0)
-z_1 = sl.sidebar.slider(label="P1_z", min_value=-2.0, max_value=2.0, value=1.0)
-p_1 = Qs([0, x_1, y_1, z_1])
+x_2 = st.sidebar.slider(label="P2_x", min_value=-2.0, max_value=2.0, value=2.0)
+y_2 = st.sidebar.slider(label="P2_y", min_value=-2.0, max_value=2.0, value=-2.0)
+z_2 = st.sidebar.slider(label="P2_z", min_value=-2.0, max_value=2.0, value=0.0)
 
-x_2 = sl.sidebar.slider(label="P2_x", min_value=-2.0, max_value=2.0, value=2.0)
-y_2 = sl.sidebar.slider(label="P2_y", min_value=-2.0, max_value=2.0, value=-2.0)
-z_2 = sl.sidebar.slider(label="P2_z", min_value=-2.0, max_value=2.0, value=0.0)
-p_2 = Qs([0, x_2, y_2, z_2])
+o_x = st.sidebar.slider(label="Origin_x", min_value=-2.0, max_value=2.0, value=0.0)
+o_y = st.sidebar.slider(label="Origin_y", min_value=-2.0, max_value=2.0, value=0.0)
+o_z = st.sidebar.slider(label="Origin_z", min_value=-2.0, max_value=2.0, value=0.0)
 
-o_x = sl.sidebar.slider(label="Origin_x", min_value=-2.0, max_value=2.0, value=0.0)
-o_y = sl.sidebar.slider(label="Origin_y", min_value=-2.0, max_value=2.0, value=0.0)
-o_z = sl.sidebar.slider(label="Origin_z", min_value=-2.0, max_value=2.0, value=0.0)
-origin = Qs([0, o_x, o_y, o_z])
+if st.sidebar.button("Straighten"):
+    x_1 = (x_2 + o_x) / 2
+    y_1 = (y_2 + o_y) / 2
+    z_1 = (z_2 + o_z) / 2
 
-curvature = sl.sidebar.slider(
-    label="Curvature", min_value=0.9, max_value=1.1, value=1.0
+    st.sidebar.slider(label="P1_x", min_value=-2.0, max_value=2.0, value=x_1)
+    st.sidebar.slider(label="P1_y", min_value=-2.0, max_value=2.0, value=y_1)
+    st.sidebar.slider(label="P1_z", min_value=-2.0, max_value=2.0, value=z_1)
+else:
+    x_1 = st.sidebar.slider(label="P1_x", min_value=-2.0, max_value=2.0, value=1.0)
+    y_1 = st.sidebar.slider(label="P1_y", min_value=-2.0, max_value=2.0, value=1.0)
+    z_1 = st.sidebar.slider(label="P1_z", min_value=-2.0, max_value=2.0, value=1.0)
+
+curvature = st.sidebar.slider(
+    label="Curvature", min_value=0.9, max_value=1.0, value=1.0
 )
+st.sidebar.markdown("TODO: handle traingles with less than 180 degrees")
 
-vertex_p_1 = sl.sidebar.checkbox(label="vertex P1", value=False)
-vertex_p_2 = sl.sidebar.checkbox(label="vertex P2", value=False)
-vertex_origin = sl.sidebar.checkbox(label="vertex Origin", value=True)
+vertex_p_1 = st.sidebar.checkbox(label="vertex P1", value=False)
+vertex_p_2 = st.sidebar.checkbox(label="vertex P2", value=False)
+vertex_origin = st.sidebar.checkbox(label="vertex Origin", value=True)
+
+show_code = st.sidebar.checkbox("Show code", False)
+
+# Form points, do the math.
+p_1 = Q([0, x_1, y_1, z_1])
+p_2 = Q([0, x_2, y_2, z_2])
+origin = Q([0, o_x, o_y, o_z])
 
 points = (p_1, p_2, origin)
 xs = [p.x for p in points]
 ys = [p.y for p in points]
 zs = [p.z for p in points]
 
-prod = product(dif(p_1, origin), dif(p_2, origin))
+prod = add(product(dif(p_1, origin), dif(p_2, origin)), origin)
 
 # Cross product
 cx_p_1 = add(cross_q(dif(p_2, p_1), dif(origin, p_1)), p_1)
 cx_p_2 = add(cross_q(dif(origin, p_2), dif(p_1, p_2)), p_2)
-cx_origin = add(cross_q(dif(p_1, origin), dif(p_2, origin)), origin)
+cx_origin = add(product(dif(p_1, origin), dif(p_2, origin), kind="odd"), origin)
 
-sl.title("Lines and Triangles")
+# Main page.
+
+st.title("Lines and Triangles")
 
 fig2 = go.Figure()
 go.Layout(margin=dict(t=500))
@@ -78,7 +95,7 @@ if vertex_origin:
         )
     )
 
-sl.write(fig2)
+st.write(fig2)
 
 angle_p1 = rotation_angle(
     p_2, origin, origin=p_1, tangent_space_norm=curvature, degrees=True
@@ -90,12 +107,90 @@ angle_origin = rotation_angle(
     p_1, p_2, origin=origin, tangent_space_norm=curvature, degrees=True
 )
 
-sl.markdown("## Angles")
-sl.markdown(f"Origin-P1-P2: {angle_p1}")
-sl.markdown(f"P1-P2-Origin: {angle_p2}")
-sl.markdown(f"P2-Origin-P1: {angle_origin}")
-sl.markdown(f"Sum of angles: {round(angle_p1.t + angle_p2.t + angle_origin.t)}")
+st.markdown("## Angles")
+st.markdown(f"Origin-P1-P2: {angle_p1}")
+st.markdown(f"P1-P2-Origin: {angle_p2}")
+st.markdown(f"P2-Origin-P1: {angle_origin}")
+st.markdown(f"Sum of angles: {round(angle_p1.t + angle_p2.t + angle_origin.t)}")
 
-sl.markdown("## Norm Squared Values")
-sl.markdown(f"P1 * P2: {norm_squared(prod).t}")
-sl.markdown(f"P1 X P2: {norm_squared(cx_origin).t}")
+st.markdown("## Norm Squared Values")
+
+total = norm_squared(prod).t
+odd = norm_squared(cx_origin).t
+
+table = f"""Odd | Total
+--- | --- | ---
+{odd} | {total}"""
+st.markdown(f"{table}")
+
+def show_file(label: st, file_name: str, code: bool = False):
+    """
+    Utility to show contents of a file
+
+    Args:
+        label: str
+        file_name: str
+        code:
+
+    Return: None
+
+    """
+    st.markdown("&nbsp ")
+    st.markdown(f"### {label}")
+    st.write(f"{file_name}")
+    with open(f"{file_name}", "r") as file:
+        file_lines = file.readlines()
+
+    if code:
+        st.code(textwrap.dedent("".join(file_lines[1:])))
+    else:
+        st.markdown(textwrap.dedent("".join(file_lines[1:])))
+
+
+st.markdown("""# Quaternion Cross Product
+
+Div, grad, curl, dot product, and cross product were all terms coined by one
+individual studying one thing: William Rowan Hamilton in his study of
+quaternions. What all students study today is the convenient divorce granted by
+Josiah Gibbs. Vector algebra is one of the most useful toolkits in one's
+mathematical collection. Vector algebra is such a dominant force in the mind's
+eye it may not be possible to imagine different perspectives on the subject. Yet
+that is my goal - not to say vector algebra is wrong in any way since there
+always necessarily is an equivalence relation between vector algebra and
+quaternions. Rather, one may gain new insights based on the the relationships
+between terms that are simply not there due to the divorce.
+
+What is a **quaternion cross product**? When one multiplies two quaternion
+together, it is the odd part of the product: """)
+
+st.latex("A B = (A B + B A)/2 + (A B - B A)/2 = Even(A B) + Odd(A B)")
+
+st.markdown("""The quaternions cross product is the odd term. This is not the
+standard definition of a Gibbs cross product because there is no notion of the
+Even() product which happens to be a combination of the difference of a scalar
+times a scalar and vector dot product summed with scalars times vectors
+(translation: an abomination to those schooled in vector algebra).
+
+If one just focuses on the quaternion cross product, the scalar term is always
+necessarily equal to zero. Why care about the difference between zero and
+undefined as with the Gibb's cross product? One can at least try to think
+about zero unlike something undefined.
+
+In the literture on 3D rotations, the fourth factor is sometimes called "w", 
+perhaps a shorthand for what-the-f. As someone who want physics to shape my
+mathematics (and visa versa), I think of the scalar as being time, and the
+three imaginaries as space. One quaternion represents one event in space-time,
+happening now. The cross product can be used to characterize 3 events-now. If
+the three events-now are in a line, then the cross product is zero. If the three
+events-now are not colinear, then the cross product will not be zero.
+
+The angles reported are the same as one expects to see from basic geometry. The
+angles sum up to 180 for a flat space-time. I added a curvature slider that
+will report on postive curvature, but at this time not negative curvature. There
+is always more work to do.
+
+""")
+
+if show_code:
+    show_file("Streamlit Webapp code", __file__, code=True)
+    show_file("Qs.py library code", "Qs.py", code=True)
